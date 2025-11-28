419d66cfcb29c34a56c94c0a818f4bb38019a86a
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-11-29 00:29:30 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -3,20 +3,17 @@ define Device/siluo_sl3000
   DEVICE_MODEL := SL3000
   DEVICE_DTS := mt7981-siluo-sl3000
   DEVICE_TITLE := Siluo SL3000
-  SUPPORTED_DEVICES := siluo,sl3000
+  SUPPORTED_DEVICES := sl3000 siluo_sl3000
 
   # 必须的硬件驱动包
   DEVICE_PACKAGES := kmod-mt7981-wifi
 
-  # 定义生成的镜像类型
-  IMAGES := preloader.bin uboot.bin sysupgrade.bin initramfs.bin
+  # 定义生成的镜像类型（常规）
+  IMAGES := sysupgrade.bin initramfs.bin
 
   # 各镜像规则
-  IMAGE/preloader.bin := append-preloader
-  IMAGE/uboot.bin := append-uboot
   IMAGE/sysupgrade.bin := append-rootfs | gzip | append-metadata
   IMAGE/initramfs.bin := append-initramfs | gzip
 endef
 
 TARGET_DEVICES += siluo_sl3000
-
