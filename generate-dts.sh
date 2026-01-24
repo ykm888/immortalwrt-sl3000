38ebef11436259561bc0bb184918adc793ed73e7
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-01-24 09:12:48 +0800

    Update generate-dts.sh

--- generate-dts.sh
+++ generate-dts.sh
@@ -1,217 +1,167 @@
 #!/bin/sh
 
-DTS="target/linux/mediatek/dts/mt7981b-sl3000-emmc.dts"
+###############################################
+# 1. 生成 DTS：mt7981b-sl-3000-emmc.dts
+###############################################
+
+DTS="target/linux/mediatek/dts/mt7981b-sl-3000-emmc.dts"
 
 cat > "$DTS" << 'EOF'
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
 /dts-v1/;
 
-#include "mt7981b.dtsi"
+#include "mt7981.dtsi"
 
 / {
-    model = "SL3000 EMMC AX3000 Router";
-    compatible = "sl3000-emmc", "mediatek,mt7981b";
+    model = "SL-3000 eMMC bootstrap version";
+    compatible = "sl,3000-emmc", "mediatek,mt7981";
 
     aliases {
         serial0 = &uart0;
-        led-boot = &led_power;
-        led-failsafe = &led_power;
-        led-running = &led_power;
-        led-upgrade = &led_power;
+        led-boot = &status_red_led;
+        led-failsafe = &status_red_led;
+        led-running = &status_green_led;
+        led-upgrade = &status_blue_led;
     };
 
     chosen {
         stdout-path = "serial0:115200n8";
-        bootargs = "console=ttyS0,115200n8";
+        bootargs = "console=ttyS0,115200n8 root=PARTLABEL=rootfs rootwait";
     };
 
     memory {
-        device_type = "memory";
-        reg = <0x40000000 0x40000000>; /* 1GB DDR */
+        reg = <0 0x40000000 0 0x20000000>;
     };
 
     leds {
         compatible = "gpio-leds";
 
-        led_power: power {
-            label = "sl3000:green:power";
-            gpios = <&gpio 0 GPIO_ACTIVE_HIGH>;
-            default-state = "on";
+        status_red_led: led-0 {
+            label = "red:status";
+            gpios = <&pio 10 GPIO_ACTIVE_LOW>;
         };
 
-        wan {
-            label = "sl3000:green:wan";
-            gpios = <&gpio 1 GPIO_ACTIVE_HIGH>;
+        status_green_led: led-1 {
+            label = "green:status";
+            gpios = <&pio 11 GPIO_ACTIVE_LOW>;
         };
 
-        lan1 {
-            label = "sl3000:green:lan1";
-            gpios = <&gpio 2 GPIO_ACTIVE_HIGH>;
-        };
-
-        lan2 {
-            label = "sl3000:green:lan2";
-            gpios = <&gpio 3 GPIO_ACTIVE_HIGH>;
-        };
-
-        lan3 {
-            label = "sl3000:green:lan3";
-            gpios = <&gpio 4 GPIO_ACTIVE_HIGH>;
+        status_blue_led: led-2 {
+            label = "blue:status";
+            gpios = <&pio 12 GPIO_ACTIVE_LOW>;
         };
     };
 
-    keys {
+    gpio-keys {
         compatible = "gpio-keys";
 
         reset {
             label = "reset";
-            gpios = <&gpio 7 GPIO_ACTIVE_LOW>;
             linux,code = <KEY_RESTART>;
-            debounce-interval = <60>;
+            gpios = <&pio 1 GPIO_ACTIVE_LOW>;
+        };
+
+        mesh {
+            label = "mesh";
+            linux,code = <BTN_9>;
+            linux,input-type = <EV_SW>;
+            gpios = <&pio 0 GPIO_ACTIVE_LOW>;
         };
     };
 };
 
 /* UART */
-&uart0 {
-    status = "okay";
-};
+&uart0 { status = "okay"; };
 
 /* Watchdog */
-&watchdog {
-    status = "okay";
-};
+&watchdog { status = "okay"; };
 
-/* Ethernet / Switch 布局：1 WAN + 3 LAN */
+/* Ethernet + Switch */
 &eth {
     status = "okay";
 
     gmac0: mac@0 {
         compatible = "mediatek,eth-mac";
         reg = <0>;
-        phy-mode = "sgmii";
-        phy-handle = <&wan_phy>;
-        nvmem-cells = <&macaddr_factory_4>;
-        nvmem-cell-names = "mac-address";
+        phy-mode = "2500base-x";
+
+        fixed-link {
+            speed = <2500>;
+            full-duplex;
+            pause;
+        };
     };
 
     gmac1: mac@1 {
         compatible = "mediatek,eth-mac";
         reg = <1>;
-        phy-mode = "rgmii";
+        phy-mode = "2500base-x";
+
         fixed-link {
-            speed = <1000>;
+            speed = <2500>;
             full-duplex;
+            pause;
         };
     };
-};
 
-&mdio {
-    status = "okay";
-
-    wan_phy: ethernet-phy@0 {
-        reg = <0>;
-    };
-
-    lan1_phy: ethernet-phy@1 {
-        reg = <1>;
-    };
-
-    lan2_phy: ethernet-phy@2 {
-        reg = <2>;
-    };
-
-    lan3_phy: ethernet-phy@3 {
-        reg = <3>;
-    };
-};
-
-&switch {
-    status = "okay";
-
-    ports {
+    mdio: mdio-bus {
         #address-cells = <1>;
         #size-cells = <0>;
 
-        port0: port@0 {
-            reg = <0>;
-            label = "wan";
-            phy-handle = <&wan_phy>;
-        };
+        switch@0 {
+            compatible = "mediatek,mt7531";
+            reg = <31>;
+            reset-gpios = <&pio 39 GPIO_ACTIVE_LOW>;
 
-        port1: port@1 {
-            reg = <1>;
-            label = "lan1";
-            phy-handle = <&lan1_phy>;
-        };
+            ports {
+                #address-cells = <1>;
+                #size-cells = <0>;
 
-        port2: port@2 {
-            reg = <2>;
-            label = "lan2";
-            phy-handle = <&lan2_phy>;
-        };
+                port@0 { reg = <0>; label = "lan1"; };
+                port@1 { reg = <1>; label = "lan2"; };
+                port@2 { reg = <2>; label = "lan3"; };
+                port@3 { reg = <3>; label = "wan"; };
 
-        port3: port@3 {
-            reg = <3>;
-            label = "lan3";
-            phy-handle = <&lan3_phy>;
-        };
+                port@6 {
+                    reg = <6>;
+                    label = "cpu";
+                    ethernet = <&gmac0>;
+                    phy-mode = "2500base-x";
 
-        cpu_port: port@6 {
-            reg = <6>;
-            label = "cpu";
-            ethernet = <&gmac1>;
-            phy-mode = "trgmii";
-            fixed-link {
-                speed = <1000>;
-                full-duplex;
+                    fixed-link {
+                        speed = <2500>;
+                        full-duplex;
+                        pause;
+                    };
+                };
             };
         };
     };
 };
 
-/* eMMC 分区 */
+/* eMMC */
 &mmc0 {
-    status = "okay";
     bus-width = <8>;
     cap-mmc-highspeed;
+    max-frequency = <52000000>;
+    no-sd;
+    no-sdio;
     non-removable;
-    disable-wp;
+    pinctrl-names = "default", "state_uhs";
+    pinctrl-0 = <&mmc0_pins_default>;
+    pinctrl-1 = <&mmc0_pins_uhs>;
     vmmc-supply = <&reg_3p3v>;
-    vqmmc-supply = <&reg_1p8v>;
-
-    mmc0boot0: partition@0 {
-        label = "emmc-boot0";
-        reg = <0x00000000 0x01000000>;
-        read-only;
-    };
-
-    mmc0boot1: partition@1000000 {
-        label = "emmc-boot1";
-        reg = <0x01000000 0x01000000>;
-        read-only;
-    };
-
-    mmc0rpmb: partition@2000000 {
-        label = "emmc-rpmb";
-        reg = <0x02000000 0x01000000>;
-        read-only;
-    };
-
-    mmc0user: partition@3000000 {
-        label = "emmc-user";
-        reg = <0x03000000 0x3D000000>;
-    };
+    status = "okay";
 };
 
-/* SPI-NOR 分区 */
-&spi0 {
+/* SPI-NOR */
+&spi2 {
     status = "okay";
 
     flash@0 {
         compatible = "jedec,spi-nor";
         reg = <0>;
-        spi-max-frequency = <50000000>;
+        spi-max-frequency = <52000000>;
 
         partitions {
             compatible = "fixed-partitions";
@@ -251,18 +201,65 @@ cat > "$DTS" << 'EOF'
 /* WiFi */
 &wifi {
     status = "okay";
-    mediatek,mtd-eeprom = <&factory 0x0000>;
+    mediatek,mtd-eeprom = <&factory 0x0>;
 };
 
-/* 电源供给 */
-&reg_3p3v {
-    status = "okay";
-};
-
-&reg_1p8v {
-    status = "okay";
-};
+/* Power */
+&reg_3p3v { status = "okay"; };
+&reg_1p8v { status = "okay"; };
 EOF
 
 git add "$DTS"
-echo "✔ DTS 已生成"
+echo "✔ DTS 已生成（sl‑3000‑emmc 官方对齐版）"
+
+
+###############################################
+# 2. 生成 MK：filogic.mk（官方架构 + 单设备）
+###############################################
+
+MK="target/linux/mediatek/image/filogic.mk"
+
+cat > "$MK" << 'EOF'
+# SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+
+DTS_DIR := $(DTS_DIR)/mediatek
+
+define Image/Prepare
+	rm -f $(KDIR)/ubi_mark
+	echo -ne '\xde\xad\xc0\xde' > $(KDIR)/ubi_mark
+endef
+
+define Build/mt7981-bl2
+	cat $(STAGING_DIR_IMAGE)/mt7981-$1-bl2.img >> $@
+endef
+
+define Build/mt7981-bl31-uboot
+	cat $(STAGING_DIR_IMAGE)/mt7981_$1-u-boot.fip >> $@
+endef
+
+###########################################################
+#  ONLY YOUR DEVICE BELOW
+###########################################################
+
+define Device/sl-3000-emmc
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000
+  DEVICE_VARIANT := eMMC bootstrap
+  DEVICE_DTS := mt7981b-sl-3000-emmc
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7981-firmware mt7981-wo-firmware \
+	f2fsck mkf2fs automount
+
+  IMAGES := sysupgrade.bin
+  KERNEL := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += sl-3000-emmc
+
+EOF
+
+git add "$MK"
+echo "✔ MK 已生成（官方架构 + sl‑3000‑emmc）"
