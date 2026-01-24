7088a22ee3078f168fe550de293c415a2faf7416
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-01-25 00:51:48 +0800

    Update three-piece-rebuild.sh

--- sl3000-tools/three-piece-rebuild.sh
+++ sl3000-tools/three-piece-rebuild.sh
@@ -5,7 +5,7 @@ echo "=== ♻ 重建 SL3000 三件套（24.10） ==="
 
 rm -f target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7981b-sl3000-emmc.dts
 rm -f target/linux/mediatek/image/filogic.mk
-rm -f .config
+rm -f mt7981b-sl3000-emmc.config
 
 sh sl3000-tools/generate-three-piece.sh
 
