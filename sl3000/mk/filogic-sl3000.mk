c55e174ff25990af24602ce72cca399074fd51d3
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-01-29 12:06:26 +0800

    Update filogic-sl3000.mk

--- sl3000/mk/filogic-sl3000.mk
+++ sl3000/mk/filogic-sl3000.mk
@@ -3,6 +3,7 @@ define Device/sl_3000-emmc
   DEVICE_MODEL := 3000 eMMC
   DEVICE_DTS := mt7981b-sl-3000-emmc
   DEVICE_DTS_DIR := ../dts
+
   DEVICE_PACKAGES := \
 	kmod-mt7915e \
 	kmod-mt7981-firmware \
@@ -12,8 +13,12 @@ define Device/sl_3000-emmc
 	blkid blockdev fdisk \
 	f2fsck mkf2fs \
 	kmod-mmc
+
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
